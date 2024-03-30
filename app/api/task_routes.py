68e11e93eb43134616de179d2ff120fa6b828d1d
Eric Sapsford <esapsgit@gmail.com>
2024-03-30 17:20:13 -0500

    start of completed branch, beginning of completed route

--- app/api/task_routes.py
+++ app/api/task_routes.py
@@ -8,6 +8,7 @@ import re
 
 task_routes = Blueprint("tasks", __name__)
 
+
 def validation_errors_to_error_messages(validation_errors):
     """
     Simple function that turns the WTForms validation errors into a simple list
@@ -18,20 +19,23 @@ def validation_errors_to_error_messages(validation_errors):
             errorMessages.append(f'{field} : {error}')
     return errorMessages
 
-### Get all tasks for today
+# Get all tasks for today
+
+
 @task_routes.route("/today")
 @login_required
 def todays_tasks():
-    tasks = Task.query.filter(Task.due_date == str(datetime.date.today())).all()
+    tasks = Task.query.filter(
+        Task.due_date == str(datetime.date.today())).all()
     if tasks:
         res = {"tasks": [task.to_dict() for task in tasks]}
         return res
     else:
-        res = { "message": "No tasks for today"}, 200
+        res = {"message": "No tasks for today"}, 200
         return res
 
 
-### Update a task
+# Update a task
 @task_routes.route("/<int:id>/update", methods=["PUT"])
 @login_required
 def update_task(id):
@@ -50,9 +54,13 @@ def update_task(id):
         db.session.commit()
         return task_to_update.to_dict()
     if form.errors:
-        return { "errors": validation_errors_to_error_messages(form.errors) }
+        return {"errors": validation_errors_to_error_messages(form.errors)}
 
-### Delete a task
+
+# Complete a task
+@task_routes.route("/<int:id>/complete", methods=["PUT"])
+@login_required
+# Delete a task
 @task_routes.route("/<int:id>/delete", methods=["DELETE"])
 @login_required
 def delete_task(id):
@@ -60,19 +68,20 @@ def delete_task(id):
     task_to_delete = Task.query.get(id)
     section_to_update = Section.query.get(task_to_delete.section_id)
 
-    #regex optimization start
-    #--------------------------------------
+    # regex optimization start
+    # --------------------------------------
     task_order_string = section_to_update.task_order
 
-    patternBegEnd = r'^' + re.escape(str(id)) + ',|,' + re.escape(str(id)) + '$'
+    patternBegEnd = r'^' + re.escape(str(id)) + \
+        ',|,' + re.escape(str(id)) + '$'
     patternMid = r',' + re.escape(str(id)) + ','
 
     task_order_string = re.sub(patternBegEnd, "", task_order_string)
     task_order_string = re.sub(patternMid, ",", task_order_string)
 
     section_to_update.task_order = task_order_string
-    #regex optimization end
-    #--------------------------------------
+    # regex optimization end
+    # --------------------------------------
 
     db.session.delete(task_to_delete)
     db.session.commit()
@@ -84,4 +93,4 @@ def delete_task(id):
         }
         return res
     else:
-        return { "message": "Unable to delete" }, 400
+        return {"message": "Unable to delete"}, 400
